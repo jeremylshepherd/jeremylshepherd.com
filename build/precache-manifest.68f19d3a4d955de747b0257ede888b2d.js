d9f82fe19b7456003a1250a190769d78a262b564
Jeremy Shepherd <jeremyshepherd@Jeremys-MacBook-Pro.local>
2019-05-03 16:14:31 -0400

    removed build folder from gitignore

---
+++ build/precache-manifest.68f19d3a4d955de747b0257ede888b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4092a2d7645407e3076e39a7dfc858d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633e4b82886e5cd281dc",
+    "url": "/static/css/main.6ea00798.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "633e4b82886e5cd281dc",
+    "url": "/static/js/main.a0ec8d84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
